a0bb694dfe554cf9c459711c9b874e76fdbc599a
Aleksandar Zivanovic <aleksandar.zivanovic.4698@metropolitan.ac.rs>
2026-03-03 15:33:00 +0100

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> users = new List<User>();
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(users);
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            return user == null ? NotFound() : Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            user.Id = users.Count + 1;
+            users.Add(user);
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, User updated)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null) return NotFound();
+            user.Name = updated.Name;
+            user.Email = updated.Email;
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null) return NotFound();
+            users.Remove(user);
+            return NoContent();
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Name { get; set; }
+
+        [System.ComponentModel.DataAnnotations.EmailAddress]
+        public string Email { get; set; }
+    }
+}
+
+
